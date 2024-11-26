6dffa40656a8716e68c1f4b6a5d2714c45a8fb2a
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2024-11-26 10:57:33 +0100

    x

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,9 @@ import {MembersComponent} from "./pages/aboutus/members/members.component";
 import {LeadershipComponent} from "./pages/aboutus/leadership/leadership.component";
 import {BenefactorComponent} from "./pages/aboutus/benefactor/benefactor.component";
 import {ImpressumComponent} from "./pages/impressum/impressum.component";
+import {EditGalleryComponent} from "./pages/privat/edit-gallery/edit-gallery.component";
+import {MediaPageComponent} from "./pages/privat/media-page/media-page.component";
+import {MerchComponent} from "./pages/privat/merch/merch.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/startseite', pathMatch: 'full' },
@@ -19,5 +22,7 @@ export const routes: Routes = [
   { path: 'kalender', component: CalendarComponent, title: 'Termine & Social Media' },
   { path: 'galerie', component: GalleryComponent, title: 'Galerie' },
   { path: 'impressum', component: ImpressumComponent, title: 'Impressum' },
-  { path: '**', component: NotfoundComponent }
+  { path: 'private-section/gallery', component:  EditGalleryComponent},
+  { path: 'private-section/media', component: MediaPageComponent },
+  { path: 'private-section/merch', component: MerchComponent }
 ];
---
+++ frontend/src/app/pages/privat/merch/merch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merch',
+  standalone: true,
+  imports: [],
+  templateUrl: './merch.component.html',
+  styleUrl: './merch.component.scss'
+})
+export class MerchComponent {
+
+}
