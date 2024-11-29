7e5b6a508cb9e6b855a2d90a9d5c74556940dd85
ShadowMega1337 <alessandro.hauser@bbzbl-it.ch>
2024-11-29 10:45:03 +0100

    add recommendations on upload

--- frontend/src/app/pages/geheim-versteck-hihi/geheim-versteck-hihi.component.ts
+++ frontend/src/app/pages/geheim-versteck-hihi/geheim-versteck-hihi.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
@@ -6,6 +6,9 @@ import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {ClientService} from "../../service/client.service";
 import {Gallery} from "../../model/gallery";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {map, Observable, startWith} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-geheim-versteck-hihi',
@@ -17,20 +20,46 @@ import {Gallery} from "../../model/gallery";
     MatFormField,
     MatInput,
     MatButton,
-    MatLabel
+    MatLabel,
+    MatAutocomplete,
+    MatOption,
+    MatAutocompleteTrigger,
+    AsyncPipe
   ],
   templateUrl: './geheim-versteck-hihi.component.html',
   styleUrl: './geheim-versteck-hihi.component.scss'
 })
-export class GeheimVersteckHihiComponent {
+export class GeheimVersteckHihiComponent implements OnInit {
   eventForm: FormGroup;
   fileStrings: string[] = [];
+  options: string[] = [
+    'Fasnacht',
+    'Allschwiler Fasnacht',
+    'Probe',
+    'Probetag u. Messebummel',
+    'Bummel',
+    'Marschprobe'
+  ];
+  filteredOptions: Observable<string[]> | undefined;
 
   constructor(private clientService: ClientService, private fb: FormBuilder) {
     this.eventForm = this.fb.group({
       year: [2024],
-      event: ['Fasnacht']
+      event: ['']
     });
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.eventForm.get('event')?.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   onFileSelected(event: Event): void {
@@ -58,7 +87,7 @@ export class GeheimVersteckHihiComponent {
     });
 
     this.clientService.createGallery(gallery).subscribe({
-      next: (data) => console.log("nice"),
+      next: () => console.log("nice"),
       error: (error) => console.log(error)
     });
 
