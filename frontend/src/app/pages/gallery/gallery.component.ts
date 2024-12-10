7a40caa98083ec94c12c7840dbf3859d204e9c21
Tsunorai <alessandro.hauser@hlk-consulting.ch>
2024-12-10 17:13:59 +0100

    update gallery filter

--- frontend/src/app/pages/gallery/gallery.component.ts
+++ frontend/src/app/pages/gallery/gallery.component.ts
@@ -7,9 +7,11 @@ import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {MatInput} from "@angular/material/input";
 import {MatPaginator} from "@angular/material/paginator";
-import {NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatAutocomplete, MatAutocompleteTrigger} from "@angular/material/autocomplete";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-gallery',
@@ -29,7 +31,10 @@ import {MatCard, MatCardContent} from "@angular/material/card";
     FormsModule,
     MatCard,
     MatCardContent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatAutocomplete,
+    MatAutocompleteTrigger
   ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
@@ -37,28 +42,59 @@ import {MatCard, MatCardContent} from "@angular/material/card";
 })
 export class GalleryComponent implements OnInit {
   galleries: Gallery[] = [];
-  selectedYear: number;
-  event: string = 'Fasnacht';
   page: number = 0;
 
-  constructor(private clientService: ClientService) {
-    this.selectedYear = new Date().getFullYear();
+  eventForm: FormGroup;
+  eventList: string[] = [];
+  filteredOptions: Observable<string[]> | undefined;
+
+  constructor(private clientService: ClientService, private fb: FormBuilder) {
+    this.eventForm = this.fb.group({
+      year: [2024],
+      event: ['Fasnacht']
+    });
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.eventList.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+
   loadGallery(): void {
-    console.log("gallery load start")
-    this.clientService.getGallery(this.selectedYear, this.event, this.page).subscribe(
-      (data: Gallery[]) => {
-        this.galleries = data;
-        console.log("gallery loaded")
-      },
-      (error) => {
-        console.error("Fehler beim Laden der Galerie:", error);
-      }
-    );
+    const year: number = this.eventForm.get("year")?.value;
+    const event: string = this.eventForm.get("event")?.value;
+
+    if (event && event !== "" && year) {
+      console.log(`gallery load start. Year: ${year}, Event: ${event}`)
+      this.clientService.getGallery(year, event, this.page).subscribe(
+        (data: Gallery[]) => {
+          this.galleries = data;
+          console.log("gallery loaded")
+        },
+        (error) => {
+          console.error("Fehler beim Laden der Galerie:", error);
+        }
+      );
+    }
   }
 
   ngOnInit(): void {
+    this.clientService.getEventList().subscribe({
+      next: (events: string[]) => {
+        console.log(events)
+        this.eventList = events
+      },
+      error: error =>
+        console.error("Fehler beim Laden der Events, " + error)
+    });
+
+    this.filteredOptions = this.eventForm.get('event')?.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
     this.loadGallery();
   }
 }
